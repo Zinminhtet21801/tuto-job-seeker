140d9a9e231e4949458ea22da19a3f36201ade85
zetminhtin <zetminhtin@gmail.com>
2020-08-30 09:56:23 +0630

    initial commit

---
+++ app/src/main/java/com/example/android/jobseekertn/JobListAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.android.jobseekertn
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class JobListAdapter : RecyclerView.Adapter<JobListAdapter.viewHolder>() {
+    class viewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
