8d2594a79bc8fa0b4024f657835a0303f798cd9b
zetminhtin <zetminhtin@gmail.com>
2020-08-30 10:28:54 +0630

    search menu

--- app/src/main/java/com/example/android/jobseekertn/MainActivity.kt
+++ app/src/main/java/com/example/android/jobseekertn/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.android.jobseekertn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun searchJob(q: String?){
+        Toast.makeText(this,q,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        val menuItem = menu?.findItem(R.id.search)
+        val actionView = menuItem?.actionView as SearchView?
+        actionView?.let {
+            it.queryHint = "Search Position"
+            it.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    searchJob(query)
+                    menuItem?.collapseActionView()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
